110a92f785761a6fc906db1b9343806c6728e355
RRValue <rrvalue@codeschnipsel.org>
2024-02-06 10:15:53 +0100

    adopted import structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use math_lingo_impl::evaluate_math_lingo;
+use math_lingo_impl::evaluate::evaluate_math_lingo;
 
 #[proc_macro]
 pub fn math_lingo(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
